19fe4275cd6d78d7b829e353378f928605b939ff
K0unna00 <78pf4jz@code.edu.az>
2024-10-20 07:23:23 +0400

    Initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,32 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './layout/layout.component';
+import { LoginComponent } from './pages/login/login.component';
+import { MainComponent } from './pages/main/main.component';
+import { FavoritesComponent } from './pages/favorites/favorites.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { HelpComponent } from './pages/help/help.component';
+import { ProductComponent } from './pages/product/product.component';
+import { HeaderComponent } from './components/template/header/header.component';
+import { FooterComponent } from './components/template/footer/footer.component';
+import { TopLineComponent } from './components/template/top-line/top-line.component';
+import { FilterComponent } from './components/template/filter/filter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    LoginComponent,
+    MainComponent,
+    FavoritesComponent,
+    ProfileComponent,
+    HelpComponent,
+    ProductComponent,
+    HeaderComponent,
+    FooterComponent,
+    TopLineComponent,
+    FilterComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/template/top-line/top-line.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-top-line',
+  templateUrl: './top-line.component.html',
+  styleUrl: './top-line.component.scss'
+})
+export class TopLineComponent {
+
+}
